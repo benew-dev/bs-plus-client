62b662ea6d45c6423cb3761ac330d131da6e83e3
Fathismael93 <fathismael@gmail.com>
2025-10-12 08:46:33 +0300

    Handling orders

--- components/orders/ListOrders.jsx
+++ components/orders/ListOrders.jsx
@@ -47,6 +47,7 @@ const OrderItemSkeleton = () => (
  * Adapté au modèle Order sans orderStatus ni shippingInfo
  */
 const ListOrders = ({ orders }) => {
+  console.log("orders: ", orders);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [filterStatus, setFilterStatus] = useState("all");
