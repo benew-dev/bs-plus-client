7903668f78a4447d3a5eb1ada34febdeed38ccb6
Fathismael93 <fathismael@gmail.com>
2025-10-22 13:49:25 +0300

    Updating paymenType model

--- backend/models/paymentType.js
+++ backend/models/paymentType.js
@@ -1,17 +1,21 @@
 import mongoose from "mongoose";
 
 const paymentTypeSchema = new mongoose.Schema({
-  paymentName: {
+  platform: {
     type: String,
-    required: true,
+    required: [true, "La plateforme de paiement est requise"],
     enum: {
       values: ["WAAFI", "D-MONEY", "CAC-PAY", "BCI-PAY"],
       message: "Type de paiement non supporté: {VALUE}",
     },
   },
+  paymentName: {
+    type: String,
+    required: [true, "Le nom du titulaire du compte est requis"],
+  },
   paymentNumber: {
     type: String,
-    required: true,
+    required: [true, "Le numéro de compte est requis"],
   },
 });
 
